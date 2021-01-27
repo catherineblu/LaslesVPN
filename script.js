6528ca47a2ad3173bf23b199fe44f644687d7847
catherineblu <e.ropleva@gmail.com>
2021-01-27 14:23:47 +0200

    hamburger nav ready

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ const menuIcon = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar')
 
 menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle("change");
+    let isMenuVisible = navbar.style.display === "block";
+    if(isMenuVisible){
+        navbar.style.display = 'none';
+    } else{
+        navbar.style.display = 'block'
+    }
 });
